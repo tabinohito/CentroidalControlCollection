457af25f13631c575a13de2bc7f8973232e1c735
tabinohito <tako.taro.tf@tut.jp>
2023-05-16 18:46:25 +0900

    できたけどなんか思ってたのと違う

--- tests/scripts/plotTestZmpBasedMethodResults.py
+++ tests/scripts/plotTestZmpBasedMethodResults.py
@@ -2,13 +2,15 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 
 class PlotTestZmpBasedMethodResults(object):
     def __init__(self,
                  method,
                  plot_capture_point=False,
-                 plot_comp_time=False):
+                 plot_comp_time=False,
+                 plot_all_time_steps=False,):
         # Setup plot
         self.fig = plt.figure()
         plt.rcParams["font.size"] = 10
@@ -19,6 +21,9 @@ class PlotTestZmpBasedMethodResults(object):
         axis_list = ["x", "y"]
         subplot_args = [len(method_list), len(axis_list), 0]
         if plot_comp_time:
+            subplot_args[0] += 1
+
+        if plot_all_time_steps:
             subplot_args[0] += 1
 
         # Loop for methods
@@ -80,6 +85,66 @@ class PlotTestZmpBasedMethodResults(object):
             ax.set_ylabel("time [ms]".format(axis_str))
             ax.grid(axis="y")
 
+        # Plot all time steps
+        # これ気づいたけどy座標だけは歩幅をたさなきゃいけないので、片脚支持期間の歩幅を足す必要がある
+        width_= 0
+        step_list = []
+        foot_step_list = [[0.2, 0.1],[0.4, -0.1],[0.6,0.1],[0.8,-0.1],[0.6,0.1],[0.6,-0.1]]
+        select = lambda x: 'Right' if x % 2 == 0 else 'Left'
+
+        #y軸オフセットを計算する
+        for i in result_data["time"]:
+            if i < 2:
+                step_list.append("Both")
+            else:
+                f, g = math.modf(i)
+                if (0 < f and f <= 0.2) or (0.8 < f and f < 1):
+                    step_list.append("Both")
+                else:
+                    step_list.append(select(g))
+        
+        if plot_all_time_steps:
+            # Loop for methods
+            self.result_data_list = {}
+            for method_idx, method_str in enumerate(method_list):
+                # Load file
+                result_file_path = "/tmp/Test{}.txt".format(method_str)
+                self.result_data_list[method_str] = np.genfromtxt(result_file_path, dtype=None, delimiter=None, names=True)
+                result_data = self.result_data_list[method_str]
+
+                # Setup subplot
+                subplot_args[2] = len(method_list) * len(axis_list) + plot_comp_time + 1
+                ax = self.fig.add_subplot(*subplot_args)                      
+
+                ax.plot(result_data["planned_zmp_x"], self.offset(step_list,result_data["planned_zmp_y"]),
+                        color="red", label="planned ZMP")
+                if "ref_zmp_x" in result_data.dtype.names and "ref_zmp_y" in result_data.dtype.names:
+                    ax.plot(result_data["ref_zmp_x"], self.offset(step_list,result_data["ref_zmp_y"]),
+                            color="blue", linestyle="dashed", label="ref ZMP")
+
+                if "ref_zmp_min_x" in result_data.dtype.names and "ref_zmp_min_y" in result_data.dtype.names:
+                    ax.plot(result_data["ref_zmp_min_x"], self.offset(step_list,result_data["ref_zmp_min_y"]),
+                            color="yellow", linestyle="dashed", label="min ZMP")
+                    
+                if "ref_zmp_max_x" in result_data.dtype.names and "ref_zmp_max_y" in result_data.dtype.names:
+                    ax.plot(result_data["ref_zmp_max_x"], self.offset(step_list,result_data["ref_zmp_max_y"]),
+                            color="blue", linestyle="dashed", label="max ZMP")
+                    
+                ax.plot(result_data["com_pos_x"], self.offset(step_list,result_data["com_pos_y"]),
+                        color="green", label="CoM")
+                        
+                if plot_capture_point and "capture_point_x" in result_data.dtype.names and "capture_point_y" in result_data.dtype.names:
+                    ax.plot(result_data["capture_point_x"], self.offset(step_list,result_data["capture_point_y"]),
+                            color="orange", label="capture point")
+
+                # Set labels, etc.
+                ax.set_title("{}-{}".format(method_str, "x-y"))
+                ax.set_xlabel("pos {}[m]".format("x"))
+                ax.set_ylabel("pos {}[m]".format("y"))
+                ax.grid()
+                if method_idx == 0:
+                    ax.legend(loc="lower right")
+
         # Show
         # plt.tight_layout()
         if len(method_list) == 1:
@@ -89,6 +154,17 @@ class PlotTestZmpBasedMethodResults(object):
             self.fig.set_size_inches((12, 13))
             plt.subplots_adjust(left=0.06, bottom=0.10, right=0.98, top=0.96, wspace=0.2, hspace=0.7)
         plt.show()
+
+    def offset(self,step_list,data):
+        offset = []
+        for i,j in enumerate(data):
+            if step_list[i] == "Left":
+                offset.append(j+0.1)
+            elif step_list[i] == "Right":
+                offset.append(j-0.1)
+            else:
+                offset.append(j)
+        return offset
 
 
 if __name__ == "__main__":
@@ -112,6 +188,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.method == "All":
         args.method = all_methods
+
+    print(args.plot_capture_point)
+    print(args.plot_comp_time)
     plot = PlotTestZmpBasedMethodResults(method=args.method,
                                          plot_capture_point=args.plot_capture_point,
-                                         plot_comp_time=args.plot_comp_time)
+                                         plot_comp_time=args.plot_comp_time,
+                                         plot_all_time_steps=True)
